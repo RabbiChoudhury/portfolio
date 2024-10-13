d561c9e6909258093c898058210cdefd7a06ae31
RabbiChoudhury <frk.frb@gmail.com>
2024-10-13 23:22:55 +0600

    created portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+// const menuBtn = document.querySelector(".menu-icon");
+// const navLink = document.querySelector(".close");
+
+// menuBtn.addEventListener("click", () => {
+//   menuBtn.classList.toggle("nav-right-mobile");
+// });
